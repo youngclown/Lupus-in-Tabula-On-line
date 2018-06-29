021659a1973f2553aaa1a080ca8b9a2a687ec8a2
ymkim <ymkim@melong.com>
2018-06-29 16:00:30 +0900

    Lupus in Tabula Start!!

---
+++ src/main/java/lupus/in/tabula/online/OnlineApplication.java
@@ -0,0 +1,12 @@
+package lupus.in.tabula.online;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineApplication.class, args);
+    }
+}
